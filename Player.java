b9c06d9461ce663d93916ba8953c702316d40aa3
darrac121 <35918838+darrac121@users.noreply.github.com>
2021-01-04 08:21:37 +0100

    Add files via upload

--- Player.java
+++ Player.java
@@ -6,9 +6,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Player {
-    static List<HashSet<Point>> reserved = new ArrayList<HashSet<Point>>();  
-    static int N = 0;  // Nombre total des joueurs (allant de 2 à 4).
-    static Point actP = new Point(-1, -1); // Position actuel de la moto [X1,Y1]
+    static List<HashSet<Point>> reserved = new ArrayList<HashSet<Point>>(); 
+    // Nombre total des joueurs (allant de 2 à 4). 
+    static int N = 0;  
+     // Position actuel de la moto [X1,Y1]
+    static Point actP = new Point(-1, -1);
 
     //Stock le prochain mouvement avec le nombre de chemins possibles
     static class Chemin {
@@ -29,8 +31,10 @@ class Player {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        String move = "LEFT"; // Direction de début
-        String[] moves = new String[3];  // Ordre du prochain mouvement
+        // Direction de début gauche
+        String move = "LEFT"; 
+        // Ordre du prochain mouvement
+        String[] moves = new String[3];  
         boolean premierTour = true;
 
         // game loop
@@ -43,18 +47,24 @@ class Player {
                 }
             }
             for (int i = 0; i < N; i++) {
-                int X0 = in.nextInt(); // starting X coordinate of lightcycle (or -1)
-                int Y0 = in.nextInt(); // starting Y coordinate of lightcycle (or -1)
-                int X1 = in.nextInt(); // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
-                int Y1 = in.nextInt(); // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+                // starting X coordinate of lightcycle (or -1)
+                int X0 = in.nextInt(); 
+                // starting Y coordinate of lightcycle (or -1)
+                int Y0 = in.nextInt(); 
+                // starting X coordinate of lightcycle (can be the same as X0 if you play before this player)
+                int X1 = in.nextInt(); 
+                // starting Y coordinate of lightcycle (can be the same as Y0 if you play before this player)
+                int Y1 = in.nextInt(); 
                 Point p1 = new Point(X1, Y1);
                 if (premierTour) {
                     Point p0 = new Point(X0,Y0);
                     reserved.get(i).add(p0);
                 }
                 if (i == P) {
+
                     reserved.get(i).add(p1);
-                    actP.move(X1, Y1); // Déplace la moto à la position X1, Y1
+                     // Déplace la moto à la position X1, Y1
+                    actP.move(X1, Y1);
                 } else {
                     if (p1.getX() != -1) {
                         reserved.get(i).add(p1);
@@ -65,14 +75,18 @@ class Player {
                     }
                 }
             }
-            if (premierTour) { // Stock les coordonnées X0, Y0 seulement pour la première fois
+            // Stock les coordonnées X0, Y0 seulement pour la première fois
+            if (premierTour) { 
                 premierTour = false; 
-            }                         
-            moves = getCurlyMoves(move);   // Stratégie utilisé pour le mouvement de la moto, voir la fonction private static String[] getCurlyMoves(String move)
-            Vector<String> validMoves = getValidMoves(moves);  // Stock les mouvements possibles
+            }
+            // Stratégie utilisé pour le mouvement de la moto, voir la fonction private static String[] getCurlyMoves(String move)                         
+            moves = getCurlyMoves(move);  
+            // Stock les mouvements possibles 
+            Vector<String> validMoves = getValidMoves(moves);  
             if (validMoves.isEmpty()) {
                 System.err.println("Echecs...");
-            } else {    // Il y a au moins 1 mouvement possible
+            } else {    
+                // Il y a au moins 1 mouvement possible
                 Vector<String> safeMoves = getSafeMoves(validMoves);
                 move = safeMoves.firstElement();
                 System.out.println(move);
@@ -147,17 +161,20 @@ class Player {
             }
             steps.add(new Chemin(move, validCount));
         }      
-        for(Chemin step : steps) {  // Affecte à la variable safeMoves 2 ou 3 directions valides de la variable Moves
+        for(Chemin step : steps) {  
+        // Affecte à la variable safeMoves 2 ou 3 directions valides de la variable Moves
             if (step.getCount() >= 2) {
                 safeMoves.add(step.getMove());
             }
         }
-        for(Chemin step : steps) { // Affecte à la variable safeMoves 1 direction valide de Moves
+        for(Chemin step : steps) { 
+        // Affecte à la variable safeMoves 1 direction valide de Moves
             if (step.getCount() == 1) {
                 safeMoves.add(step.getMove());
             }
         }
-        for(Chemin step : steps) { // Affecte à la variable safeMoves 0 direction valide de la variable Moves.
+        for(Chemin step : steps) { 
+        // Affecte à la variable safeMoves 0 direction valide de la variable Moves.
             if (step.getCount() == 0) {
                 safeMoves.add(step.getMove());
             }
@@ -184,9 +201,12 @@ class Player {
         boolean match = true;
         for (int[] rule : rules) {
             Point p = new Point(rule[0], rule[1]);
-            if (rule[2] == 0) {                         // Vérifie si est libre
+            // Vérifie si est libre
+            if (rule[2] == 0) {                         
                 if (!onGrid(p) || isReserved(p)) { match = false; }
-            } else {                                    // Vérifie si est occupé
+            } else
+            // Vérifie si est occupé
+            {                                    
                 if (onGrid(p) && !isReserved(p)) { match = false; }
             }
         }
@@ -197,36 +217,29 @@ class Player {
         int x = (int)(actP.getX());
         int y = (int)(actP.getY());
         boolean turn = false;
-        // Règle pour détecter Uturn dans la direction d’un mouvement. Uturn est atteint si les 9 règles correspondent
+        // Règle pour détecter isUturn dans la direction d’un mouvement. 
         // Valeur : coordX, cordY, 0 correspond à libre et 1 à occupé
-        int[][] leftRulesA  = new int[][] { {x,y+1,0}, {x-1,y+1,0}, {x-1,y,0}, {x,y-1,1}, {x-1,y-1,1},
-                                            {x-2,y,1}, {x-2,y+1,1}, {x-1,y+2,1}, {x,y+2,1} };
-        int[][] leftRulesB  = new int[][] { {x,y-1,0}, {x-1,y-1,0}, {x-1,y,0}, {x,y-2,1}, {x-1,y-2,1},
-                                            {x-2,y-1,1}, {x-2,y,1}, {x-1,y+1,1}, {x,y+1,1} };
-        int[][] rightRulesA = new int[][] { {x,y-1,0}, {x+1,y-1,0}, {x+1,y,0}, {x,y-2,1}, {x+1,y-2,1},
-                                            {x+2,y-1,1}, {x+2,y,1}, {x+1,y+1,1}, {x,y+1,1} };
-        int[][] rightRulesB = new int[][] { {x,y+1,0}, {x+1,y+1,0}, {x+1,y,0}, {x,y-1,1}, {x+1,y-1,1},
-                                            {x+2,y,1}, {x+2,y+1,1}, {x+1,y+2,1}, {x,y+2,1} };                                            
-        int[][] upRulesA    = new int[][] { {x,y-1,0}, {x+1,y,0}, {x+1,y-1,0}, {x-1,y,1}, {x-1,y-1,1},
-                                            {x,y-2,1}, {x+1,y-2,1}, {x+2,y-1,1}, {x+2,y,1} };
-        int[][] upRulesB    = new int[][] { {x-1,y,0}, {x-1,y-1,0}, {x,y-1,0}, {x-2,y,1}, {x-2,y-1,1},
-                                            {x-1,y-2,1}, {x,y-2,1}, {x+1,y-1,1}, {x+1,y,1} };
-        int[][] downRulesA  = new int[][] { {x,y+1,0}, {x+1,y,0}, {x+1,y+1,0}, {x-1,y,1}, {x-1,y+1,1},
-                                            {x,y+2,1}, {x+1,y+2,1}, {x+2,y+1,1}, {x+2,y,1} };
-        int[][] downRulesB  = new int[][] { {x-1,y,0}, {x-1,y+1,0}, {x,y+1,0}, {x-2,y,1}, {x-2,y+1,1},
-                                            {x-1,y+2,1}, {x,y+2,1}, {x+1,y+1,1}, {x+1,y,1} };
+        int[][] leftA  = new int[][] { {x,y+1,0}, {x-1,y+1,0}, {x-1,y,0}, {x,y-1,1}, {x-1,y-1,1}, {x-2,y,1}, {x-2,y+1,1}, {x-1,y+2,1}, {x,y+2,1} };
+        int[][] leftB  = new int[][] { {x,y-1,0}, {x-1,y-1,0}, {x-1,y,0}, {x,y-2,1}, {x-1,y-2,1}, {x-2,y-1,1}, {x-2,y,1}, {x-1,y+1,1}, {x,y+1,1} };
+        int[][] rightA = new int[][] { {x,y-1,0}, {x+1,y-1,0}, {x+1,y,0}, {x,y-2,1}, {x+1,y-2,1}, {x+2,y-1,1}, {x+2,y,1}, {x+1,y+1,1}, {x,y+1,1} };
+        int[][] rightB = new int[][] { {x,y+1,0}, {x+1,y+1,0}, {x+1,y,0}, {x,y-1,1}, {x+1,y-1,1}, {x+2,y,1}, {x+2,y+1,1}, {x+1,y+2,1}, {x,y+2,1} };                                            
+        int[][] upA    = new int[][] { {x,y-1,0}, {x+1,y,0}, {x+1,y-1,0}, {x-1,y,1}, {x-1,y-1,1}, {x,y-2,1}, {x+1,y-2,1}, {x+2,y-1,1}, {x+2,y,1} };
+        int[][] upB    = new int[][] { {x-1,y,0}, {x-1,y-1,0}, {x,y-1,0}, {x-2,y,1}, {x-2,y-1,1}, {x-1,y-2,1}, {x,y-2,1}, {x+1,y-1,1}, {x+1,y,1} };
+        int[][] downA  = new int[][] { {x,y+1,0}, {x+1,y,0}, {x+1,y+1,0}, {x-1,y,1}, {x-1,y+1,1}, {x,y+2,1}, {x+1,y+2,1}, {x+2,y+1,1}, {x+2,y,1} };
+        int[][] downB  = new int[][] { {x-1,y,0}, {x-1,y+1,0}, {x,y+1,0}, {x-2,y,1}, {x-2,y+1,1}, {x-1,y+2,1}, {x,y+2,1}, {x+1,y+1,1}, {x+1,y,1} };
+        //choisit les directions
         switch(move) {
             case "LEFT":
-                if (isRulesMatch(leftRulesA) || isRulesMatch(leftRulesB)) { turn = true; }
+                if (isRulesMatch(leftA) || isRulesMatch(leftB)) { turn = true; }
                 break;
             case "RIGHT":
-                if (isRulesMatch(rightRulesA) || isRulesMatch(rightRulesB)) { turn = true; }
+                if (isRulesMatch(rightA) || isRulesMatch(rightB)) { turn = true; }
                 break;
             case "UP":
-                if (isRulesMatch(upRulesA) || isRulesMatch(upRulesB)) { turn = true; }
+                if (isRulesMatch(upA) || isRulesMatch(upB)) { turn = true; }
                 break;
             case "DOWN":
-                if (isRulesMatch(downRulesA) || isRulesMatch(downRulesB)) { turn = true; }
+                if (isRulesMatch(downA) || isRulesMatch(downB)) { turn = true; }
                 break;
         }
         return turn;
